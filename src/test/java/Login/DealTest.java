9335e8240d0dff4c43a29bd4183c63e17be2e04d
User <User@User-PC>
2018-03-27 20:02:18 +0530

    Push

--- src/test/java/Login/DealTest.java
+++ src/test/java/Login/DealTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class DealTest extends loginTest {
@@ -16,6 +17,11 @@ public class DealTest extends loginTest {
 	WebElement ele= driver.findElement(By.xpath("//a[text()='Deals']"));
 	act.moveToElement(ele).build().perform();
 	driver.findElement(By.xpath("//a [text()='New Deal']")).click();
+	driver.findElement(By.id("title")).sendKeys("SLK SOFT");
+	driver.findElement(By.id("(//input[@type='text'])[1]")).sendKeys("Software solutions");
+	WebElement ele1=driver.findElement(By.xpath("//select[@name='type']"));
+	  Select sel=new Select(ele1);
+	  sel.selectByVisibleText("Old");
 	  
 	 
   }
